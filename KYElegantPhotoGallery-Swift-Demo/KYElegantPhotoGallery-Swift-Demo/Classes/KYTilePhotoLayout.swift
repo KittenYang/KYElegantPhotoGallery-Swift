b9a3bf7190f645ce0e644117c76812f8572c96e7
KittenYang <kittenyang@icloud.com>
2015-06-06 03:54:17 +0800

    first commit

---
+++ KYElegantPhotoGallery-Swift-Demo/KYElegantPhotoGallery-Swift-Demo/Classes/KYTilePhotoLayout.swift
@@ -0,0 +1,13 @@
+//
+//  KYTilePhotoLayout.swift
+//  KYElegantPhotoGallery-Swift-Demo
+//
+//  Created by Kitten Yang on 6/6/15.
+//  Copyright (c) 2015 Kitten Yang. All rights reserved.
+//
+
+import UIKit
+
+class KYTilePhotoLayout: UICollectionViewLayout {
+   
+}
